8e483f0a0f1d1282d30cf614ddeabee322955aa3
Levi Lewis <levi@calmdev.com>
2017-04-24 10:25:51 -0400

    Adds props to listing model.

--- app/data/models/Listing.js
+++ app/data/models/Listing.js
@@ -2,6 +2,8 @@ module.exports = (store, server) => {
 
   const Listing = store.createModel('listings', {
     id: store.type.string(),
+    active: store.type.boolean(),
+    amenities: store.type.array(),
     city: store.type.string(),
     description: store.type.string(),
     state: store.type.string(),
