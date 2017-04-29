a2ac7d7c23d66e7136e2e1886733a9158ab7c6cc
Levi Lewis <levi@calmdev.com>
2017-04-29 14:11:56 -0400

    adds task to delete stale listings

--- app/cron/controller.js
+++ app/cron/controller.js
@@ -17,6 +17,30 @@ const internals = {
             listings
           }
         }));
+    },
+    'delete-stale-listings': (request, reply) => {
+
+      const taskName = request.params.taskName;
+      const store = request.server.plugins.data.store();
+      return store.Listing
+        .getJoin({ uploads: true })
+        .filter((listing) => listing('active').eq(false))
+        .filter((listing) => listing('expiresAt').lt(store.r.now().sub(86400 * 30))) // 30 days
+        .run()
+        .then((listings) => {
+
+          return Promise.all((listings).map((listing) => {
+
+            return listing.deleteAll()
+              .then((listing) => listing.purge());
+          }));
+        })
+        .then((listings) => ({
+          name: taskName,
+          work: {
+            listings
+          }
+        }));
     }
   }
 };
